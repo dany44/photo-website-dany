b5b7b7e8cf39c98193526398b0aebcdaad26ca6b
dany44 <danykhadhar@hotmail.fr>
2025-03-18 13:13:29 +0100

    feat: album name now displayed

--- frontend/src/pages/Album/AlbumGalleryPage.jsx
+++ frontend/src/pages/Album/AlbumGalleryPage.jsx
@@ -1,23 +1,22 @@
 // src/pages/Album/AlbumGalleryPage.jsx
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import PhotoGallery from '../../components/gallery/PhotoGallery';
+import { AlbumContext } from '../../context/AlbumContext';
 
 function AlbumGalleryPage() {
   const { albumId } = useParams();
+  const { albums } = useContext(AlbumContext);
+
+  const album = albums.find((a) => a._id === albumId);
+  const albumName = album ? album.name : "Album Inconnu";
 
   return (
     <div className="p-4 text-white min-h-screen">
-      <h2 className="text-2xl font-bold mb-4">Album</h2>
+      <h2 className="text-2xl font-bold mb-4">{albumName}</h2>
 
       {/* On injecte albumId dans PhotoGallery */}
       <PhotoGallery albumId={albumId} isAdmin={true} />
-      {/* 
-         - "isAdmin" selon tes besoins, 
-         - Par ex. si l’utilisateur est vraiment admin
-           tu peux récupérer ça depuis AuthContext au lieu 
-           de mettre isAdmin={true} en dur.
-      */}
     </div>
   );
 }
