72e9eb1208206913d3815148901b4946545ae9bd
dany44 <danykhadhar@hotmail.fr>
2025-03-19 17:21:11 +0100

    feat: Persistant cache in local storage added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,36 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; 
-import App from './App'; 
-import './index.css'; 
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+
+// Créez votre QueryClient
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-//  <React.StrictMode>
-<QueryClientProvider client={queryClient}>
-    <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-    </AuthProvider>
-    </QueryClientProvider>
+// Créez un persister utilisant le localStorage
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
- // </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistQueryClientProvider
+    client={queryClient}
+    persistOptions={{ persister: localStoragePersister }}
+    onSuccess={() => {
+      console.log('Cache restauré !');
+    }}
+  >
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </PersistQueryClientProvider>
 );
