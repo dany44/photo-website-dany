0c6dcd1d7134fed5ee91df57671ef3fbde081651
dany44 <danykhadhar@hotmail.fr>
2025-03-20 17:01:09 +0100

    fix: debug prod env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const config = require('./config/Config'); 
+const config = require('./config/Config');
 const photoRoutes = require('./routes/photoRoutes');
 const albumRoutes = require('./routes/albumRoutes');
 const path = require('path');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
-const cookieParser = require('cookie-parser'); 
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -16,12 +16,14 @@ const app = express();
 config.connectDB();
 
 // Middleware pour autoriser les requêtes CORS
-const allowedOrigins = process.env.FRONTEND_URL 
+const allowedOrigins = process.env.FRONTEND_URL
   ? process.env.FRONTEND_URL.split(',')
   : ['http://localhost:3001'];
 
+console.log("🚀 FRONTEND_URL chargé sur Railway :", process.env.FRONTEND_URL);
+
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
     // En production, on rejette les requêtes sans origine
     if (!origin) {
       if (process.env.NODE_ENV === 'production') {
@@ -57,22 +59,22 @@ app.use('/photos', photoRoutes);
 app.use('/albums', albumRoutes);
 // Route de test
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur l\'API Photo Gallery !');
+  res.send('Bienvenue sur l\'API Photo Gallery !');
 });
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Gestion des erreurs globales
 app.use((err, req, res, next) => {
-    config.log('error', `${err.message} - ${req.method} ${req.originalUrl}`);
-    res.status(err.status || 500).json({
-        message: err.message || 'Erreur interne du serveur.',
-    });
+  config.log('error', `${err.message} - ${req.method} ${req.originalUrl}`);
+  res.status(err.status || 500).json({
+    message: err.message || 'Erreur interne du serveur.',
+  });
 });
 
 // Démarrage du serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    config.log('info', `Serveur démarré sur http://localhost:${PORT}`);
+  config.log('info', `Serveur démarré sur http://localhost:${PORT}`);
 });
 
