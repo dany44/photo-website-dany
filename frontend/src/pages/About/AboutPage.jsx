b118b947dfba2e6cbbc19760546ddcebb1b239f6
dany44 <danykhadhar@hotmail.fr>
2025-03-22 16:06:33 +0100

    feat: Prevent users to copy images

--- frontend/src/pages/About/AboutPage.jsx
+++ frontend/src/pages/About/AboutPage.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
                     À Propos de Moi
                 </h1>
                 <p className="text-xl leading-relaxed mb-8">
-                    Je m'appelle Dany Khadhar. Amateur de photographie de voyage, j'ai créé ce site pour partager mes clichés réalisés lors de mes déplacements. Mon approche reste simple et personnelle, centrée sur l'expérience et l'authenticité de chaque moment.
+                    Je m'appelle Dany Khadhar. Amateur de photographie, j'ai créé ce site pour partager mes clichés réalisés lors de mes déplacements. Attiré par la nature, la culture et les modes de vie authentiques, je rassemble ici mes photos, réalisées avec une approche simple et personnelle, mettant en avant l'authenticité de chaque instant.
                 </p>
                 <section className="mb-8">
                     <h2 className="text-3xl font-bold mb-4">Matériel</h2>
@@ -18,13 +18,13 @@ const AboutPage = () => {
                             <strong>Sony a6400</strong> – Un appareil compact et léger, idéal pour mes voyages.
                         </li>
                         <li>
-                            <strong>Sony E PZ 16-50mm f/3.5-5.6 OSS</strong> – Un objectif polyvalent, parfait pour les prises de vue en grand-angle au quotidien.
+                            <strong>Sony E PZ 16-50mm f/3.5-5.6 OSS</strong> – Un objectif polyvalent, pour les prises de vue en grand-angle au quotidien.
                         </li>
                         <li>
                             <strong>Sigma 56mm f/1.4 DC DN Contemporary</strong> – Un objectif fixe qui me permet de capturer des portraits et de travailler en basse lumière.
                         </li>
                         <li>
-                            <strong>Sony FE 70-350mm f/4.5-6.3 G OSS</strong> – Un téléobjectif pratique pour photographier des sujets éloignés lors de mes déplacements.
+                            <strong>Sony FE 70-350mm f/4.5-6.3 G OSS</strong> – Un téléobjectif pour photographier des sujets éloignés.
                         </li>
                     </ul>
                 </section>
