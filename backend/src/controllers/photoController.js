b591d6774721da45864924efc43b0fc74b06c013
dany44 <danykhadhar@hotmail.fr>
2025-03-22 15:45:47 +0100

    fix: minor console fix

--- backend/src/controllers/photoController.js
+++ backend/src/controllers/photoController.js
@@ -48,7 +48,7 @@ exports.login = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'None',
             maxAge: 3600000, // 1h
         });
 
